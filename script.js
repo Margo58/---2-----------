841d1f5c0b06655dc6b065f8bfbb3319df6cbc73
Margo584 <mfirs.mara666@gmail.com>
2023-04-02 20:04:07 +0300

    выполнила дз4

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let a = 10;
 alert (20);
 
-v = 2007;
+let v = 2007;
 alert(v);
 
 let product = "Брендон Айком";
 alert(product);
+
+let age = 10;
+let up = 2;
+let result = age + up;
+alert(result);
+
+let b = 10;
+let c = 2;
+let d = b / c;
+alert(d);
+
