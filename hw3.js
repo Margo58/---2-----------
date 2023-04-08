d1e78610360780fe577105ff1f7deb71dbb0c87f
Margo584 <mfirs.mara666@gmail.com>
2023-04-08 20:31:18 +0300

    задание 2

--- hw3.js
+++ hw3.js
@@ -4,6 +4,22 @@ let age = prompt('Введите пароль');
 if (age >= 444) {
     password = 'Пароль введен верно';
 } else {
-    password = 'Пароль введен не верно';
+    password = 'Пароль введен неправильно';
 }
 alert(password);
+
+const c = 0;
+c >= 0 && c <= 10 ?
+console.log('Верно') : console.log('Неверно');
+
+const n = 10;
+n >= 0 && n <= 10 ?
+console.log('Верно') : console.log('Неверно');
+
+const i = -3;
+i >= 0 && i <= 10 ?
+console.log('Верно') : console.log('Неверно');
+
+const p = 10;
+p >= 0 && p <= 10 ?
+console.log('Верно') : console.log('Неверно');
